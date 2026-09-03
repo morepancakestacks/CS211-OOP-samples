dc075a650ff11ad3f7485475b49551dc1979d8d4
morepancakestacks <agdonmarie@gmail.com>
2026-09-03 15:12:51 +0800

    updates on code samples

--- Lesson 2/Conditional Structures/CheckLetter.java
+++ Lesson 2/Conditional Structures/CheckLetter.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class CheckLetter{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a character: ");
         char ch = input.next().charAt(0);
         if((ch == 'A' && ch <= 'Z') || (ch == 'a' && ch == 'z')){
             System.out.println(ch + " is a letter.");
--- Lesson 2/Iterative Structures/PascalTriangle.java
+++ Lesson 2/Iterative Structures/PascalTriangle.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
-class PascalTriangle{
-    public static void main(String[]args){
-        int rows;
-        Scanner row = new Scanner(System.in);
 
-        System.out.print("Enter the number of rows: ");
-        rows = row.nextInt();
+class PascalTriangle {
+    public static void main(String[]args) {
+        Scanner input = new Scanner(System.in);
 
-        for(int i = 0; i < rows; i++){
-            int coef = 1; 
-            for(int j = 0; j <= i; j++){
-                System.out.print(coef +" ");
-                coef = coef * (i-j)/(j+1);
-            }System.out.print("\n");
-        } 
-        row.close();
-    } 
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+
+            int number = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%4d", number);
+                //System.out.print(number + " ");
+
+                number = number * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+
+        input.close();
+    }
 }
